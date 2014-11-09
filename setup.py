6a7113ece1529286296e33410bd008ebf339575e
Vitalik Buterin <v@buterin.com>
2014-11-09 11:01:14 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='spread',
+      version='1.1.19',
+      description='CSV Spreadsheet library',
+      author='Vitalik Buterin',
+      author_email='v@buterin.com',
+      url='http://github.com/vbuterin/spread',
+      packages=['spread'],
+      data_files=[("", ["LICENSE"])]
+      )
